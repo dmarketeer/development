076e38219b8f52b08835b61c0d4f707e783e2bcb
dmarketeer <130474034+dmarketeer@users.noreply.github.com>
2025-11-10 08:48:36 +0000

    Delete oportunidades directory

--- oportunidades/public/js/public.js
+++
@@ -1,8 +0,0 @@
-( function() {
-    document.addEventListener( 'click', function( event ) {
-        const target = event.target;
-        if ( target.matches( '.oportunidades-table a' ) ) {
-            target.setAttribute( 'rel', 'noopener noreferrer' );
-        }
-    } );
-} )();
